727a984b3fa100442359b6a33c085d8061c38c7a
khyungjun <kpos7503@naver.com>
2023-11-03 18:15:48 +0900

    Init Project

---
+++ src/main/java/com/inflearn/jpashop/InflearnJpaShopApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJpaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnJpaShopApplication.class, args);
+	}
+
+}
